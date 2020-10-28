01fb189c7de5705edd84191df93fb5589ad07317
Ruslan Hlushan <ruslan.hlushan@gmail.com>
2020-10-28 09:42:59 +0300

    init

---
+++ app/src/main/java/com/android/academy2020/homework3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.academy2020.homework3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homework_3_solution)
+    }
+}
